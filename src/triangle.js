175b7cb1df5f471d52d644ff74164f418893aa4e
marnionrails <37088724+marnionrails@users.noreply.github.com>
2020-12-13 12:02:52 -0500

    complete tests

--- src/triangle.js
+++ src/triangle.js
@@ -5,5 +5,14 @@
 }
 
 Triangle.prototype.checkType = function() {
-    return "not a triangle";
-} 
+    if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+      return "not a triangle";
+    } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
+      return "scalene triangle";
+    }  else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
+      return "equilateral triangle";
+    } else {
+      return "isosceles triangle";
+    }
+  };
+  
